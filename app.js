fab6652dce2a5787078b9e3c5b92a4f04deaf690
Sergio <36644639+sergiogar99@users.noreply.github.com>
2019-11-28 17:26:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,279 +1,88 @@
 import { MongoClient, ObjectID } from "mongodb";
-import { GraphQLServer } from "graphql-yoga";
-import *as uuid from 'uuid';
-
 import "babel-polyfill";
 
-//"mongodb+srv://sergio:123pez@cluster0-dikpx.gcp.mongodb.net/test?retryWrites=true&w=majority"
+const uri = "mongodb+srv://sergio:123pez@cluster0-dikpx.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-const usr = "sergio";
-const pwd = "123pez";
-const url = "cluster0-dikpx.gcp.mongodb.net/test?retryWrites=true&w=majority";
-
-/**
- * Connects to MongoDB Server and returns connected client
- * @param {string} usr MongoDB Server user
- * @param {string} pwd MongoDB Server pwd
- * @param {string} url MongoDB Server url
- */
-const connectToDb = async function(usr, pwd, url) {
-  const uri = `mongodb+srv://${usr}:${pwd}@${url}`;
+client.connect(err => {
   
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+  const db = client.db("recetario");
+  const recetas = db.collection("recetas");
+  const autores = db.collections("authors");//Si no existe la crea
+  //const id;
 
-  await client.connect();
-  return client;
-};
+  recetas.insertOne(
+    {
+      name : "Brownie",
+      author:"1",
+      description:"molt be",
+      id:"1"
+    })
+    .then((result)=> {
+      console.log(result.ops);
+      id = result.ops[0]._id;
+      console.log(_id);
+      return recetas.updateOne({_id:id},{$set:{name:"Tarta de Queso"}})
+      .then (() => console.log("modificado con exito"));
+    });
 
-/**
- * Starts GraphQL server, with MongoDB Client in context Object
- * @param {client: MongoClinet} context The context for GraphQL Server -> MongoDB Client
- */
-const runGraphQLServer = function(context) {
-  const typeDefs = `
-    type Query{
+  // perform actions on the collection object
+  client.close();
 
-     getFacturas(name:String!,token:ID!):[Facturas]
-    }
-
-    type Mutation{
-
-      addUser(name: String!, password: String!):Titulares!
-      addFacturas(name:String!,token:ID!,fecha:String!,concepto:String!,cantidad:Float!):Facturas!
-
-      removeUser(name:String!,token:ID!):Titulares
-      removeFacturas(name:String!,token:ID!,id:ID!):Facturas
-
-      login(name:String!,password:String!):ID!
-      logout(name:String!,password:String!):String!
-    }
-
-    type Titulares{
-      _id: ID!
-      name: String!
-      password: String!
-      token:ID!
-      facturas:[Facturas]
-    }
-
-    type Facturas{
-
-      _id:ID!
-
-      idFactura:ID!
-
-      fecha: String!
-      concepto: String!
-      cantidad: Float!
-      titular: Titulares!
-    }
-
-  `;
-
-  //el id de la factura es el id del titular
-
-  const resolvers = {
-
-    // Titulares:{
-
-    //   facturas:async (parent, args, ctx, info)=>{
-  
-    //     const rec= parent.title;
-    //     const { client } = ctx;
-    //     const db = client.db("blog");
-    //     const collection = db.collection("ingredients");
-
-    //     const result = await collection.find({"recipe": rec}).toArray();        
-    //     return result;
-    //   },
-  
-
-    
-    // },
-
-    // Facturas:{
+});
 
 
-    // },
+// import "babel-polyfill";
 
-  
-    Query: {
- 
-      getFacturas: async (parent, args, ctx, info) => {
+// const uri =
+//   "mongodb+srv://avalero:123456abc@cluster0-e8ug9.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-        const { name,token } = args;
-        const { client } = ctx;
-        const db = client.db("morosos");
-        const collection = db.collection("users");
+// const databaseName = "blog";
+// const id = new ObjectID();
+// console.log(id);
+// console.log(id.getTimestamp());
 
-        //buscar el usuario por username
-        //obtener su token y comparar por el que se ha añadido
-        //devolver lista de facturas por id, comparandolo por el id del usuario
-        //las facturas tienen el id del user
+// client.connect(async err => {
+//   if (err) {
+//     return console.log(`Error connecting to ${uri}`);
+//   }
+//   console.log("Connected!");
+//   const db = client.db(databaseName);
+//   const collection = db.collection("authors");
 
-        // const result = await collection.find({}).toArray();
-        // return result;
+//   const result = await collection.insertOne({
+//     name: "Alberto",
+//     age: 41
+//   });
 
-        
+//   console.log(result.ops);
 
-        const exist = await collection.findOne({name:name});
+//   const _id = result.ops._id;
 
-        if(exist){
+//   const result2 = await collection.insertOne({
+//     name: "Pedro",
+//     age: 41
+//   });
 
-          console.log("Existe bri"+ exist.name + exist.token);
-        }
+//   const updateOne = await collection.updateOne(
+//     { _id },
+//     { $set: { name: "Andres" } }
+//   );
+//   console.log(`update result: ${updateOne.result.ok}`);
 
-        if(exist.token == token){
+//   const updateMany = await collection.updateMany(
+//     { name: "Alberto" },
+//     { $set: { name: "Luis" } }
+//   );
 
-          const collection = db.collection("facturas");
+//   console.log(`update many: ${updateMany.result.nModified}`);
 
-          const result = await collection.find({"idFactura": exist._id}).toArray(); 
+//   const remove = collection.deleteMany({ name: "Luis" });
 
-          return result;
-        }
-
-      },
-      
-    },
-
-    Mutation: {
-
-      addUser: async (parent, args, ctx, info) => {
-
-        const { name, password } = args;
-        const { client } = ctx;
-        const db = client.db("morosos");
-        const collection = db.collection("users");
-
-        const exist = await collection.findOne({name:name});
-
-        if(!exist){
-
-          const token = uuid.v4();
-
-          const result = await collection.insertOne({ name, password ,token});
-
-          return {
-            name,
-            password,
-            token,
-            _id: result.ops[0]._id
-          };
-        }
-
-      },
-      addFacturas: async (parent, args, ctx, info) => {
-
-        const { name, token,fecha,concepto,cantidad } = args;
-        const { client } = ctx;
-        const db = client.db("morosos");
-        let collection = db.collection("users");
-
-        const exist = await collection.findOne({name:name});
-
-        if(exist.token == token){
-
-          collection = db.collection("facturas");
-          const idFactura = exist._id;
-          const result = await collection.insertOne({ fecha, concepto ,cantidad,idFactura});
-
-          return {
-            idFactura,
-            fecha,
-            concepto,
-            cantidad,
-            _id: result.ops[0]._id
-          }; 
-
-        }
-        
-      },
-      login: async (parent, args, ctx, info) => {
-        const { name, password } = args;
-        const { client } = ctx;
-        const db = client.db("blog");
-        const collection = db.collection("authors");
-        const result = await collection.insertOne({ name, mail });
-
-        return {
-          name,
-          mail,
-          _id: result.ops[0]._id
-        };
-      },
-      logout: async (parent, args, ctx, info) => {
-        const { name, password } = args;
-        const { client } = ctx;
-        const db = client.db("blog");
-        const collection = db.collection("authors");
-        const result = await collection.insertOne({ name, mail });
-
-        return {
-          name,
-          mail,
-          _id: result.ops[0]._id
-        };
-      },
-
-
-      removeUser: async (parent, args, ctx, info) => {
-        const { name, password } = args;
-        const { client } = ctx;
-        const db = client.db("blog");
-        const collection = db.collection("authors");
-        const result = await collection.insertOne({ name, mail });
-
-        return {
-          name,
-          mail,
-          _id: result.ops[0]._id
-        };
-      },
-      removeFacturas: async (parent, args, ctx, info) => {
-        const { name, password } = args;
-        const { client } = ctx;
-        const db = client.db("blog");
-        const collection = db.collection("authors");
-        const result = await collection.insertOne({ name, mail });
-
-        return {
-          name,
-          mail,
-          _id: result.ops[0]._id
-        };
-      },
-    }
-  };
-
-  const server = new GraphQLServer({ typeDefs, resolvers, context });
-  const options = {
-    port: 8001
-  };
-
-  try {
-    server.start(options, ({ port }) =>
-      console.log(
-        `Server started, listening on port ${port} for incoming requests.`
-      )
-    );
-  } catch (e) {
-    console.info(e);
-    server.close();
-  }
-};
-
-const runApp = async function() {
-  const client = await connectToDb(usr, pwd, url);
-  console.log("Connect to Mongo DB");
-  try {
-    runGraphQLServer({ client });
-  } catch (e) {
-    console.log(e);
-    client.close();
-  }
-};
-
-runApp();
+//   // perform actions on the collection object
+//   client.close();
+// });
